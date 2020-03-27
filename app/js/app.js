3d59979079fdcc811761e56658ca9414ffe9acc1
Alex Nabzdorov <Sanek_109@list.ru>
2020-03-27 21:38:22 +0300

    first gulp

---
+++ app/js/app.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    let nameForm = document.querySelector('#form-name');
+    let phoneForm = document.querySelector('#form-number-phone');
+    let buttonForm = document.querySelector('#button-get-form');
+
+    buttonForm.addEventListener('click', () => {alert('You enter button!')})
+    //
+    // buttonForm.onClick = () => {
+    //     alert('You enter button!')
+    // }
+
+
+
+
+
+});
+
+
